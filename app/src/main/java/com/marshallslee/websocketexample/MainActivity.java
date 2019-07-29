28eefc03f5bfd9c56289b2f3bfc6ae45f248de40
Marshall Lee <marshall.s.lee@gmail.com>
2019-07-29 15:02:51 +0900

    websocket

--- app/src/main/java/com/marshallslee/websocketexample/MainActivity.java
+++ app/src/main/java/com/marshallslee/websocketexample/MainActivity.java
@@ -6,33 +6,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.marshallslee.websocketexample.keys.Keys;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
 
-import java.net.URISyntaxException;
-
-import io.socket.client.IO;
-import io.socket.client.Socket;
-import io.socket.emitter.Emitter;
-import io.socket.engineio.client.EngineIOException;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements WebSocketEcho.WebSocketInteractor {
     private final String TAG = getClass().getSimpleName();
     private EditText etFirstName, etLastName;
-
-    private Socket socket;
-    private boolean isConnected = true;
+    private WebSocket mWebSocket = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connect();
+        WebSocketEcho.getInstance(this);
 
         Button btnQuery = findViewById(R.id.btnQuery);
         etFirstName = findViewById(R.id.etFirstName);
@@ -41,93 +32,24 @@ public class MainActivity extends AppCompatActivity {
         btnQuery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    callMessage();
+                String firstName = etFirstName.getText().toString();
+                String lastName = etLastName.getText().toString();
+                callMessage(firstName, lastName);
             }
         });
     }
 
-    private void connect() {
-        try {
-            socket = IO.socket(Keys.BASE_URL);
-            socket.on(Socket.EVENT_CONNECT, onConnect);
-            socket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
-            socket.on(Socket.EVENT_DISCONNECT, onDisconnect);
-            socket.on(Keys.RESPONSE, onMessageReceived);
-            socket.connect();
-        } catch(URISyntaxException e) {
-            Log.e(TAG, "URISyntaxException caught: " + e.getMessage());
-        }
+    private void callMessage(String firstName, String lastName) {
+        mWebSocket.send(firstName);
     }
-
-    private void callMessage() {
-        JSONObject data = new JSONObject();
-        String firstName = etFirstName.getText().toString();
-        String lastName = etLastName.getText().toString();
-        if(socket.connected()) {
-            try {
-                data.put(Keys.FIRST_NAME, firstName);
-                data.put(Keys.LAST_NAME, lastName);
-                socket.emit(Keys.NAME, data);
-                Log.e(TAG, "Emitting the data: " + data.toString());
-            } catch (JSONException e) {
-                Log.e(this.getClass().getSimpleName(), "Exception: " + e.getMessage());
-                e.printStackTrace();
-            }
-        } else {
-            Log.e(TAG, "Socket is not connected.");
-        }
-    }
-
-    private Emitter.Listener onMessageReceived = args -> runOnUiThread(() -> {
-        JSONObject receivedData = (JSONObject) args[0];
-        String hello;
-        try {
-            hello = receivedData.getString(Keys.NAME);
-            Toast.makeText(MainActivity.this, hello, Toast.LENGTH_SHORT).show();
-        } catch(JSONException e) {
-            Log.e(TAG, "JSONException caught: " + e.getMessage());
-        }
-    });
-
-    private final Emitter.Listener onConnect = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            Log.e(TAG, "Websocket is successfully connected.");
-        }
-    };
-
-    private Emitter.Listener onConnectError = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    EngineIOException e = (EngineIOException) args[0];
-                    Log.e(TAG, "Failed to connect  " + e.getMessage());
-                }
-            });
-        }
-    };
-
-    private Emitter.Listener onDisconnect = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(MainActivity.this, "Disconnected.", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    };
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e(TAG, "turning the socket off.");
-        socket.disconnect();
-        socket.off(Socket.EVENT_CONNECT, onConnect);
-        socket.off(Socket.EVENT_CONNECT_ERROR, onConnectError);
-        socket.off(Socket.EVENT_DISCONNECT, onDisconnect);
+    public void onOpen(WebSocket webSocket) {
+        mWebSocket = webSocket;
+    }
+
+    @Override
+    public void onGetMessage(String message) {
+
     }
 }
--- app/src/main/java/com/marshallslee/websocketexample/keys/Keys.java
+++ app/src/main/java/com/marshallslee/websocketexample/keys/Keys.java
@@ -1,7 +1,7 @@
 package com.marshallslee.websocketexample.keys;
 
 public class Keys {
-    public static final String BASE_URL = "http://15.164.25.217:12381/";
+    public static final String BASE_URL = "http://15.164.25.217:3010/";
     public static final String RESPONSE = "response";
     public static final String NAME = "name";
     public static final String FIRST_NAME = "firstName";
